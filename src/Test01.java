a53808cdcec75d5c87e196e4b000725fd5ab3b9e
unknown <x15g005@PC505N024.chiba-fjb.ac.jp>
2015-09-04 12:03:14 +0900

    修正３

--- src/Test01.java
+++ src/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("test");
-		System.out.println("test02");
+		System.out.println("テスト");
 
 	}
 
